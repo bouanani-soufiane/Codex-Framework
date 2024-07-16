1005b99259b68790dd7bcd3a64c978a6544cc89a
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-07-16 22:53:20 +0100

    switch from gradle to maven | gradle couldnt load burningwave correctly

---
+++ src/main/java/com/soufinane/Main.java
@@ -0,0 +1,38 @@
+package com.soufinane;
+
+import org.burningwave.core.assembler.ComponentContainer;
+import org.burningwave.core.assembler.ComponentSupplier;
+import org.burningwave.core.classes.ClassCriteria;
+import org.burningwave.core.classes.ClassHunter;
+import org.burningwave.core.classes.SearchConfig;
+
+import java.util.Collection;
+public class Main {
+    public static void main(String[] args) {
+        Collection<Class<?>> classes = Main.find();
+        Main.printClassNames(classes);
+
+        System.out.println("here : " + classes.getClass());
+    }
+
+    public static Collection<Class<?>> find() {
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        ClassHunter classHunter = componentSupplier.getClassHunter();
+
+        SearchConfig searchConfig = SearchConfig.byCriteria(
+                ClassCriteria.create().allThoseThatMatch((cls) -> {
+                    return cls.getPackage().getName().matches(".*springframework.*");
+                })
+        );
+
+        try (ClassHunter.SearchResult searchResult = classHunter.findBy(searchConfig)) {
+            return searchResult.getClasses();
+        }
+    }
+
+    public static void printClassNames(Collection<Class<?>> classes) {
+        for (Class<?> cls : classes) {
+            System.out.println(cls.getName());
+        }
+    }
+}
